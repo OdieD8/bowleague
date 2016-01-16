bc50d9287a4075f220404d13a1f700f5f26b09d0
OdieD8 <odie_de_moura@hotmail.com>
2016-01-16 13:47:23 -0700

    .gitignore update

---
+++ core/server/config/env/passport.js
@@ -0,0 +1,28 @@
+var JwtStrategy = require("passport-jwt").Strategy;
+
+var User = require("../../features/users/user.server.model");
+var config = require("./development");
+
+module.exports = function(passport) {
+
+	var opts = {};
+	opts.secretOrKey = config.sessionSecret;
+	passport.use(new JwtStrategy(opts, function(jwt_payload, done) {
+
+		User.findOne({id: jwt_payload.id }, function(err, user) {
+
+			if (err) {
+
+				return done(err, false);
+			}
+			if (user) {
+
+				done(null, user);
+			}
+			else {
+
+				done(null, false);
+			}
+		});
+	}));
+};

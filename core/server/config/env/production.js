2a11458aa44959365297e78bd96d7895644da95b
Odie de Moura <odie_de_moura@hotmail.com>
2016-01-16 13:45:28 -0700

    Delete production.js

--- core/server/config/env/production.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-    // db: 'mongodb://username:password@ds031903.mongolab.com:31903/example',
-    // sessionSecret: 'productionSecret'
-
-    db: "mongodb://OdieD8:janete454@ds056698.mongolab.com:56698/bowling-stats",
-    sessionSecret: "productionSecret"
-};
